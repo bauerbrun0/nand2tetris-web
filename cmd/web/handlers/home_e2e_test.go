0d896a9a7fcf871e48a802ece82fa97b7f825663
bauerbrun0 <bauerbruno@protonmail.com>
2025-08-08 14:21:23 +0200

    test: add e2e tests for home handler

---
+++ cmd/web/handlers/home_e2e_test.go
@@ -0,0 +1,31 @@
+package handlers_test
+
+import (
+	"net/http"
+	"testing"
+
+	modelsmocks "github.com/bauerbrun0/nand2tetris-web/internal/models/mocks"
+	servicemocks "github.com/bauerbrun0/nand2tetris-web/internal/services/mocks"
+	"github.com/bauerbrun0/nand2tetris-web/internal/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHome(t *testing.T) {
+	githubOauthService := servicemocks.NewMockOAuthService(t)
+	googleOauthService := servicemocks.NewMockOAuthService(t)
+	queries := modelsmocks.NewMockDBQueries(t)
+
+	ts := testutils.NewTestServer(t, queries, githubOauthService, googleOauthService, false)
+	defer ts.Close()
+
+	t.Run("Can visit page unauthenticated", func(t *testing.T) {
+		code, _, _ := ts.Get(t, "/")
+		assert.Equal(t, http.StatusOK, code)
+	})
+	t.Run("Can visit the page authenticated", func(t *testing.T) {
+		ts.MustLogIn(t, queries, "walt", "walter.white@example.com", "LosPollos321")
+		code, _, body := ts.Get(t, "/")
+		assert.Equal(t, http.StatusOK, code)
+		assert.Contains(t, body, "walter.white@example.com")
+	})
+}
