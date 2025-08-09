a0a82dae5fec6b2ec4aa32a636ec5280029961b6
bauerbrun0 <bauerbruno@protonmail.com>
2025-08-09 22:10:25 +0200

    test: split test utils into dedicated files

--- internal/testutils/testserver.go
+++ internal/testutils/testserver.go
@@ -10,7 +10,6 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"os"
-	"regexp"
 	"strings"
 	"testing"
 	"time"
@@ -416,24 +415,4 @@ func (ts *testServer) MustRegister(t *testing.T, queries *mocks.MockDBQueries, u
 
 	postResult := ts.PostForm(t, "/user/register", form)
 	assert.Equal(t, http.StatusSeeOther, postResult.Status)
-}
-
-var rxCSRF = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+?)">`)
-
-func ExtractCSRFToken(t *testing.T, body string) string {
-	matches := rxCSRF.FindStringSubmatch(body)
-	if len(matches) < 2 {
-		t.Fatalf("no csrf token found in body")
-	}
-
-	return matches[1]
-}
-
-func MustHashPassword(t *testing.T, hasher crypto.PasswordHasher, password string) string {
-	t.Helper()
-	hash, err := hasher.GenerateFromPassword(password, crypto.DefaultPasswordHashParams)
-	if err != nil {
-		t.Fatalf("error generating hash for password %q: %v", password, err)
-	}
-	return hash
 }
---
+++ internal/testutils/utils.go
@@ -0,0 +1,28 @@
+package testutils
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/bauerbrun0/nand2tetris-web/internal/crypto"
+)
+
+var rxCSRF = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+?)">`)
+
+func ExtractCSRFToken(t *testing.T, body string) string {
+	matches := rxCSRF.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatalf("no csrf token found in body")
+	}
+
+	return matches[1]
+}
+
+func MustHashPassword(t *testing.T, hasher crypto.PasswordHasher, password string) string {
+	t.Helper()
+	hash, err := hasher.GenerateFromPassword(password, crypto.DefaultPasswordHashParams)
+	if err != nil {
+		t.Fatalf("error generating hash for password %q: %v", password, err)
+	}
+	return hash
+}
