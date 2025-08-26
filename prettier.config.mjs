99dfeee7fd2b0df0c9e554057048523dcd713434
bauerbrun0 <bauerbruno@protonmail.com>
2025-08-26 15:13:14 +0200

    chore: add templ prettier plugin

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,22 @@
 import pluginSvelte from "prettier-plugin-svelte";
 
 export default {
-  plugins: [pluginSvelte, "prettier-plugin-tailwindcss"],
+  plugins: [
+    pluginSvelte,
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-templ-script",
+  ],
   overrides: [
     {
       files: "*.svelte",
       options: {
         parser: "svelte",
+      },
+    },
+    {
+      files: ["*.templ"],
+      options: {
+        parser: "templ",
       },
     },
   ],
