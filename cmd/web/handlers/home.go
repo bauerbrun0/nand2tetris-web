dc91eb8f110eecee112cce50e3fbdced7265c342
bauerbrun0 <bauerbruno@protonmail.com>
2025-10-28 19:15:51 +0100

    feat: redirect authenticated users from / to /project

--- cmd/web/handlers/home.go
+++ cmd/web/handlers/home.go
@@ -3,14 +3,13 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/bauerbrun0/nand2tetris-web/ui/pages/homepage"
 	"github.com/bauerbrun0/nand2tetris-web/ui/pages/landingpage"
 )
 
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	data := h.NewPageData(r)
 	if h.IsAuthenticated(r) {
-		h.Render(r.Context(), w, r, homepage.Page(data))
+		http.Redirect(w, r, "/projects", http.StatusSeeOther)
 		return
 	}
 	h.Render(r.Context(), w, r, landingpage.Page(data))
