fd6f25d87447e3342a7a6df684b807fd56a04290
bauerbrun0 <bauerbruno@protonmail.com>
2026-05-30 11:32:17 +0200

    fix: use forwarded client IP in request logs

---
+++ cmd/web/middleware/helpers.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+func GetClientIp(r *http.Request) string {
+	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
+		parts := strings.Split(xff, ",")
+		return strings.TrimSpace(parts[0])
+	}
+
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+
+	return host
+}
